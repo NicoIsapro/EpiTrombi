293214eaa6a8ad10b080781a5c949d43eb7cab6c
Nico Isapro <nicotropchou@hotmail.fr>
2018-02-12 18:31:54 +0100

    MAJ importante
    Suite à une modification du code de réponse epitech, voilà une nouvelle version !

--- stalker.py
+++ stalker.py
@@ -51,12 +51,19 @@ if username.find('.') == -1:
 # ----------------------------------------------------#
 #                      CHECKS                         #
 # ----------------------------------------------------#
+headers = {
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
+}
 
 if extension == ".bmp":
 	print ("Marvin: I will check if HD is available for your m8, It can take a minute\n")
 	url = "https://cdn.local.epitech.eu/userprofil/" + username + extension
-	response = requests.get(url)
-	if response != 200:
+	response = requests.get(url, headers=headers)
+	if response.status_code != 200:
 		print ("Marvin: 404 not found lol --> I try with standard mode\n ")
 		extension = ".jpg"
 	else:
@@ -70,8 +77,8 @@ if extension == ".bmp":
 
 print ("Marvin: I will check if your m8 exists\n")
 url = "https://cdn.local.epitech.eu/userprofil/profilview/" + username + extension
-response = requests.get(url)
-if response != 200:
+response = requests.get(url, headers=headers)
+if response.status_code != 200:
 	print ("Marvin: 404 not found lol\n")
 	exit()
 else:
